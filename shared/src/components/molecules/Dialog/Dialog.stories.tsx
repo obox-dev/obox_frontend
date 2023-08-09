fc95675264e35af12786b354c4c6c3153aa80507
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2023-08-09 17:18:34 +0200

    storybook for dialog added

---
+++ shared/src/components/molecules/Dialog/Dialog.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Dialog } from "./Dialog";
+
+const meta: Meta<typeof Dialog> = {
+  title: "Story system/Dialog",
+  component: Dialog,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DialogExample: Story = {
+  args: {
+    okCallback: () => {
+      console.log("OK pressed");
+    },
+    cancelCallback: () => {
+      console.log("Cancel pressed");
+    },
+    title: "This is a dialog",
+    size: 'lg',
+    okText: 'OK',
+    cancelText: 'Close',
+    children: <span>Dialog text</span>
+  },
+};
