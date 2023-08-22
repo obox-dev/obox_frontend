a90d7e57ca2097dc907f51a74ed1dd3cd6be6062
ThorSR <ThorSR@MacBook-Pro-Macbook.local>
2023-08-22 13:07:49 +0300

    language localisation added

--- admin/src/pages/Page_404/NotFound.tsx
+++ admin/src/pages/Page_404/NotFound.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import { useTranslation } from "@libs/react-i18next";
 import { Button, ButtonVariants } from '@shared/components/atoms/Button';
-import './NotFound.scss'
+import './NotFound.scss';
 
 const NotFound: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className="not-found-404">404</div>
-        <div className='message'><h1>Ууупс!</h1>
-          <p className='p'>Наш шеф все ще працює над приготуванням цієї сторінки. Пропонуємо поки спробувати інші.</p>
-          <div className='button-group'>
-            <Button text="Повернутися на попередню сторінку" variant={ButtonVariants.SUCCESS} onClick={() => window.history.back()} />
-            <div style={{ width: '20px' }}></div>
-            <Button text="На головну" variant={ButtonVariants.PRIMARY} onClick={() => window.location.href = '/'} />
-          </div>
+      <div className="message">
+        <h1>{t('common:notFoundTitle')}</h1>
+        <p className="p">{t('common:notFoundMessage')}</p>
+        <div className="button-group">
+          <Button
+            text={t('common:prevPageButton')}
+            variant={ButtonVariants.SUCCESS}
+            onClick={() => window.history.back()}
+          />
+          <div style={{ width: '20px' }}></div>
+          <Button
+            text={t('common:homeButton')}
+            variant={ButtonVariants.PRIMARY}
+            onClick={() => (window.location.href = '/')}
+          />
         </div>
+      </div>
     </div>
   );
 };
