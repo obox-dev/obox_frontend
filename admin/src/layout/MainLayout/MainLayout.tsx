23b4009144d5810b21bf156c8c45621264dd879b
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2023-09-01 16:56:16 +0200

    comment resolved

--- admin/src/layout/MainLayout/MainLayout.tsx
+++ admin/src/layout/MainLayout/MainLayout.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { Header } from '../../components/organisms/Header';
-import { Footer } from '../../components/organisms/Footer';
-import Home from '../../pages/Home/Home';
-import Restaurants from '../../pages/Restaurants/Restaurants';
-import { MenuPage } from '../../pages/Menu/MenuPage';
-import NotFound from '../../pages/Page_404/NotFound';
-import { MenuDishPage } from '../../pages/Menu/components/MenuDish/MenuDishPage';
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Header } from "../../components/organisms/Header";
+import { Footer } from "../../components/organisms/Footer";
+import Home from "../../pages/Home/Home";
+import Restaurants from "../../pages/Restaurants/Restaurants";
+import { MenuPage } from "../../pages/Menu/MenuPage";
+import NotFound from "../../pages/Page_404/NotFound";
+import { MenuDishPage } from "../../pages/Menu/components/MenuDish/MenuDishPage";
 
 export const MainLayout: React.FC = () => {
   const currentYear = new Date().getFullYear();
@@ -20,13 +20,21 @@ export const MainLayout: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/restaurants" element={<Restaurants />} />
           <Route path="/menu" element={<MenuPage />} />
-          <Route path="/menu/:menuId" element={<MenuPage/>} />
-          <Route path="/menu/:menuId/category/:categoryId" element={<MenuPage/>} />
-          <Route path="/menu/:menuId/category/:categoryId/create-dish" element={<MenuDishPage/>} />
-          <Route path="/menu/:menuId/category/:categoryId/dish/:dishId" element={<MenuDishPage/>} />
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="/menu/:menuId" element={<MenuPage />} />
+          <Route
+            path="/menu/:menuId/category/:categoryId"
+            element={<MenuPage />}
+          />
+          <Route
+            path="/menu/:menuId/category/:categoryId/create-dish"
+            element={<MenuDishPage />}
+          />
+          <Route
+            path="/menu/:menuId/category/:categoryId/dish/:dishId"
+            element={<MenuDishPage />}
+          />
           <Route path="/not-found" element={<NotFound />} />
-            <Route path="*" element={<Navigate replace to="/not-found" />} />
+          <Route path="*" element={<Navigate replace to="/not-found" />} />
         </Routes>
       </main>
       <Footer currentYear={currentYear} />
