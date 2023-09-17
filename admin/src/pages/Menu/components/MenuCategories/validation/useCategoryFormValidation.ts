44bba912ac3628974de666f2dab37724ba18e527
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2023-09-17 15:45:53 +0200

    variables for validation added

--- admin/src/pages/Menu/components/MenuCategories/validation/useCategoryFormValidation.ts
+++ admin/src/pages/Menu/components/MenuCategories/validation/useCategoryFormValidation.ts
@@ -6,24 +6,26 @@ export const useCategoryFormValidation = () => {
   const MIN_NAME_LENGTH = 1;
   const MAX_NAME_LENGTH = 200;
 
+  const NAME_IS_REQUIRED_MESSAGE = t('common:validation:isRequired', {
+    field: t('dishForm:name'),
+  });
+
+  const MIN_NAME_LENGTH_MESSAGE = t('common:validation:morethan', {
+    field: t('common:name'),
+    min: MIN_NAME_LENGTH,
+  });
+
+  const MAX_NAME_LENGTH_MESSAGE = t('common:validation:lessthan', {
+    field: t('common:name'),
+    max: MAX_NAME_LENGTH,
+  });
+
   const validationSchema = yup.object().shape({
     name: yup
       .string()
-      .required(t('common:validation:isRequired', { field: t('common:name') }))
-      .min(
-        MIN_NAME_LENGTH,
-        t('common:validation:morethan', {
-          field: t('common:name'),
-          min: MIN_NAME_LENGTH,
-        })
-      )
-      .max(
-        MAX_NAME_LENGTH,
-        t('common:validation:lessthan', {
-          field: t('common:name'),
-          max: MAX_NAME_LENGTH,
-        })
-      )
+      .required(NAME_IS_REQUIRED_MESSAGE)
+      .min(MIN_NAME_LENGTH, t(MIN_NAME_LENGTH_MESSAGE))
+      .max(MAX_NAME_LENGTH, t(MAX_NAME_LENGTH_MESSAGE))
       .trim(),
   });
   return { validationSchema };
