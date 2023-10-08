375c7cff67886b71e576e226e569b9757d5c3199
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2023-10-08 19:52:25 +0200

    in stock fuild added

---
+++ admin/src/components/atoms/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+// import { Switcher } from '@shared/components/atoms/Switcher';
+// import { useState } from 'react';
+
+// enum ThemeOptions {
+//   LIGHT = 'light',
+//   DARK = 'dark',
+// }
+
+// const themeNames = {
+//   [ThemeOptions.LIGHT]: 'light',
+//   [ThemeOptions.DARK]: 'dark',
+// };
+
+// const defaulTheme = ThemeOptions.LIGHT;
+
+export const ThemeSwitcher = () => {
+//   const [currentTheme, setCurrentTheme] = useState(defaulTheme);
+//   const onThemeChange = (e: any) => {
+//       console.log(e)
+//     if (true) {
+//     }
+//   };
+//   return <Switcher onChange={onThemeChange} name="theme" />;
+};
