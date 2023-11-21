e2abde7eee19e106fd3c1e9d86bac4cfde89a911
Yaroslav Andriievskyi <yandriievskyi@ascendix.net>
2023-11-21 14:02:56 +0100

    reverted watcher for input component

--- shared/src/components/atoms/Input/Input.tsx
+++ shared/src/components/atoms/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+// import { useMemo } from 'react';
 import { useFormInput } from '@shared/hooks/useFormInput';
 import { IInput, InputVariants } from './types';
 import './Input.scss';
@@ -26,14 +26,8 @@ export const Input = (props: IInput<HTMLInputElement>) => {
   };
 
   const options = { ...(onChange ? { onChange } : {}) };
-  const { ref, watch, registerParams, error } = useFormInput(name, options);
+  const { ref, registerParams, error } = useFormInput(name, options);
   const inputClassName = combineClasses(type, !!error);
-
-  const watchedValue = watch?.(name);
-
-  const innerValue = useMemo(() => {
-    return value || watchedValue;
-  }, [value, watchedValue]);
 
   return (
     <>
@@ -41,7 +35,7 @@ export const Input = (props: IInput<HTMLInputElement>) => {
         onChange={onChange}
         id={id}
         ref={ref}
-        value={innerValue}
+        value={value}
         name={name}
         type={type}
         className={inputClassName}
