9725dbc52d5e01d597c7b4a7f613914b6c3821cd
ptrodion <rdnpetrenko@gmail.com>
2023-12-03 21:12:51 +0000

    add all export for Marks in index.ts

--- admin/src/pages/Tags/pages/Marks/components/MarkPage/hooks/index.ts
+++ admin/src/pages/Tags/pages/Marks/components/MarkPage/hooks/index.ts
@@ -1,1 +1,4 @@
 export { useGetMarks } from './useGetMarks';
+export { useCreateMarks } from './useCreateMarks';
+export { useDeleteMarks } from './useDeleteMarks';
+export { useUpdateMarks } from './useUpdateMarks';
